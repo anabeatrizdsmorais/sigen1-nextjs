7427def6e95f5bed3a2ec01697b6614902311567
Ana B Morais <anabeatrizdsmorais@gmail.com>
2022-06-02 17:44:43 -0300

    Primeiro commit - Sigen1 com nextjs

---
+++ pages/header.js
@@ -0,0 +1,12 @@
+import styles from '../styles/Header.module.css'
+
+export default function Header() {
+	return (
+		<div className={styles.container}>
+			<div className={styles.page-header}></div>
+		</div>
+	)
+}
+
+// export default Vendas;
+
